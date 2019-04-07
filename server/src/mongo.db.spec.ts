1ab8a79bc61f15b5b4138c4167ddcbd0e08c1a03
Piotr Dajlido <piotr.dajlido@valuestreams.pl>
2019-04-07 17:52:03 +0200

    Removed shadowed variables from spec

--- server/src/mongo.db.spec.ts
+++ server/src/mongo.db.spec.ts
@@ -2,12 +2,6 @@ import {MongoDB} from './mongo.db';
 
 describe('Mongo DB', () => {
 
-  let db: MongoDB;
-
-  beforeEach(() => {
-    db = new MongoDB();
-  });
-
   it('can connect to DB', async () => {
     const db = new MongoDB();
     return db.onConnected;
